c407b9ceb43c61ac76db1a55911442f0a0587294
Javier Tamez <tamezjavier@gmail.com>
2022-01-28 15:23:58 -0600

    Created some logic to test for Webpack behavior.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GhostChat from './GamePage/GhostChat.jsx';
 
-
+//Using a random code to test react rendering correctly.
 ReactDOM.render(
-  <div>Hello Shady Wolves!</div>,
-  document.getElementById('app')
+  <GhostChat></GhostChat>,
+  document.getElementById('root')
 );

a083b0b45e29e981c576a3b76d1edd66bc1cdabe
Megan Wolf <meganwolf@megans-mbp.attlocal.net>
2022-01-28 19:59:31 -0500

    Set up basic components

--- src/LandingPage/WaitingToStart.jsx
+++ src/LandingPage/WaitingToStart.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const CreateGameModal = () => {
+const WaitingToStart = () => {
 
 
   return (
@@ -12,4 +12,4 @@ const CreateGameModal = () => {
 
 }
 
-export default CreateGameModal;
+export default WaitingToStart;

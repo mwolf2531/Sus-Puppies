1a42779113156a661cf375e2feff129e46bd17c0
Javier Tamez <tamezjavier@gmail.com>
2022-01-29 17:33:47 -0600

    Saving current work to push up to Himmat.

--- src/GamePage.jsx
+++ src/GamePage.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import data from './mockData.js';
+import { gameState, playerState } from './mockData.js';
+import PlayerFrame from './Components/PlayerFrame.jsx';
+import Ruleset from './Components/Ruleset.jsx';
 import Header from './Components/Header.jsx';
 import LivingChat from './Components/LivingChat.jsx';
-import Ruleset from './Components/Ruleset.jsx';
+import GhostChat from './Components/GhostChat.jsx';
+import WolfChat from './Components/WolfChat.jsx';
+import Timer from './Components/Timer.jsx';
 import GameButton from './Components/GameButton.jsx';
 import Voting from './Components/Voting.jsx';
-import WolfChat from './Components/WolfChat.jsx';
-import GhostChat from './Components/GhostChat.jsx';
-import Timer from './Components/Timer.jsx';
-import PlayerFrame from './Components/PlayerFrame.jsx';
 
 
 
@@ -17,16 +17,13 @@ const GamePage = () => {
 
   //Note: These states are not final in anyway.
   const [time, setTime] = useState(0);
-  const [centerDisplay, setCenterDisplay] = useState({});
-  const [phaseResult, setPhaseResult] = useState({
-    message: 'Dare died last night',
-    currentDay: 'day',
-  });
-  const [currentDay, setCurrentDay] = useState('day');
-  const [gameRules, setGameRules] = useState({});
-  const [allPlayerInfo, setAllPlayerInfo] = useState({});
-  const [gameLogic, setGameLogic] = useState({});
-  const [votingRecord, setVotingRecord] = useState({});
+  const [previousResult, setPreviousResult] = useState('');
+  const [currentDay, setCurrentDay] = useState(0);
+  const [currentPhase, setCurrentPhase] = useState('');
+  const [playerRoles, setPlayerRoles] = useState([]);
+  const [voting, setVoting] = useState([]);
+  const [gameStatus, setGameStatus] = useState('');
+  const [phaseResults, setPhaseResults] = useState([]);
 
   //TODO: Add Lifecycle methods as needed.
 

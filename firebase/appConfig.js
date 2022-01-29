bc24b5e4a3f7fdd1682826644e0c8f7443c57f19
Daniel Gonzalez <dboyg123.dg@gmail.com>
2022-01-29 14:52:54 -0500

    backend scaffolding

--- firebase/appConfig.js
+++ firebase/appConfig.js
@@ -30,6 +30,7 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 function writeUserData(name, email) {
+  // create game state
   set(ref(db), {
     gameState: {
       user: name,
@@ -61,3 +62,76 @@ async function updateUserEmail(username, email, nickname) {
 }
 
 updateUserEmail('user', 'newEmail@example.com', 'dogman');
+
+// Routes:==========================================
+
+const phaseCycle = () => {};
+// phase cycle
+// if end of game
+// set game status = ended
+// update previous result to 'gameover' or whatever
+// else
+// cycle phase
+// reset chat for wolves and villagers (leave ghost)
+// reset timer
+// day -> night or night -> day
+// if night -> day, increment day counter
+// update previous result
+// clear voting
+
+const routes = {
+  joinGame: () => {},
+  // join game
+  // create host if playerinfo array is empty
+  // if playerinfo length greater than one
+  // push player  into playerInfo array
+  startGame: () => {},
+  // start game
+  // set game status from setup to playing
+  // user submits 'x' number of wolves
+  // set 'x' players to wolf roles randomly
+  // reset gamestate
+  // timer 90
+  // day = 0
+  // clear previous results
+  // clear all chats
+  // reset phaseresults array
+  pauseGame: () => {},
+  // pause game
+  // set game status to pause
+  // if host changes state to pause or if player disconnected
+  resumeGame: () => {},
+  // resume game
+  // set game status to playing
+  ghostChatSubmit: () => {},
+  // ghostChat => submit message
+  // takes id, message
+  // store as name and message (based on playerInfo data)
+  villagerChatSubmit: () => {},
+  // living chat => submit message
+  wolfChatSubmit: () => {},
+  // wolf chat => submit message
+  playerVoteSubmit: () => {},
+  // player votes => submit vote
+  // check for all votes
+  // if not all votes send game state
+  // check for majority
+  // kill player on majority
+  // phase cycle
+  // send state
+  wolfVoteSubmit: () => {},
+  // wolf votes => submit vote
+  // SEER && HEALER LOGIC
+  // wolves have to vote
+  // if null
+  // kill random (any villager)
+  // if one out of two total votes
+  // kill only vote
+  // on tie,
+  // random between both votes submitted
+  // majority
+  // kill submitted majority
+  // update phaseResults array
+  // phase cycle
+  // send
+};
--- src/mockData.js
+++ src/mockData.js
@@ -1,5 +1,9 @@
-const chatData = [['host', 'Were no strangers to love'], ['megan', 'You know the rules and so do I'],
-['himmat', 'A full commitments what Im thinking of'], ['Ryder123', 'Really guys?']];
+const chatData = [
+  ['host', 'Were no strangers to love'],
+  ['megan', 'You know the rules and so do I'],
+  ['himmat', 'A full commitments what Im thinking of'],
+  ['Ryder123', 'Really guys?'],
+];
 
 const data = {
   gameState: {
@@ -7,20 +11,35 @@ const data = {
     previousResult: 'Dare died last night!',
     currentDay: 0,
     currentPhase: 0, // use an integer to represent current day. 0 === Day. 1 === Night.
-    playerRoles: [['host', 0], ['megan',0], ['himmat', 0], ['Ryder123',2]],
-    voting: [['host', 'megan'], ['megan', 'megan'], ['himmat', 'megan'], ['Ryder123', 'megan']],
+    playerRoles: [
+      ['host', 0],
+      ['megan', 0],
+      ['himmat', 0],
+      ['Ryder123', 2],
+    ],
+    voting: [
+      ['host', 'megan'],
+      ['megan', 'megan'],
+      ['himmat', 'megan'],
+      ['Ryder123', 'megan'],
+    ],
     gameStatus: 'playing',
     phaseResults: [1, 'Dare died last night!'], //How and where.
-    playerInfo: [{name: 'Dare', picture: 'todo/pic'}, {name: 'Himmat', picture: 'todo/pic'}, {name: 'Megan', picture: 'todo/pic'}, {name: 'Ryder', picture: 'todo/pic'}],
+    playerInfo: [
+      { name: 'Dare', picture: 'todo/pic' },
+      { name: 'Himmat', picture: 'todo/pic' },
+      { name: 'Megan', picture: 'todo/pic' },
+      { name: 'Ryder', picture: 'todo/pic' },
+    ],
     ghostChats: chatData,
     livingChats: chatData,
     wolfChats: chatData,
-    host: 'host' // This is a string that will match host's id which is also a string.
+    host: 'host', // This is a string that will match host's id which is also a string.
   },
   playerState: {
     player_id: 'host', // Asked to be a string. It could be a weird string like this: '12bx#sdr311'
   },
-}
+};
 //Note: First player to log in will be mapped to host property in the incoming data.
 
 export default data;

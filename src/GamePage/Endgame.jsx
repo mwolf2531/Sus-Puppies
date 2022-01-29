fcabd35ce69606a003dc06e04ecddc92d5ba90e9
Himmat Khalsa <himmatsinghkhalsa@gmail.com>
2022-01-28 18:11:51 -0700

    add timer, endgame and living chat components

---
+++ src/GamePage/Endgame.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import Styled from 'styled-components';
+
+const Endgame = () => {
+  return (
+    <div>
+      <h2>Werewolves Won!</h2>
+      <div>The endgame condition was that werewolves outnumber villagers</div>
+      <div>Player List:</div>
+      <div>player1</div>
+      <div>player2</div>
+      <div>player3</div>
+      <div>player4</div>
+      <div>player5</div>
+    </div>
+  )
+}
+
+export default Endgame;

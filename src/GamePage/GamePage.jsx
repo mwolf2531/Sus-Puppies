d5267fcb90ff6572369d4203ae42a284b83da081
Javier Tamez <tamezjavier@gmail.com>
2022-01-28 20:02:04 -0600

    Implemented wireframe for GamePage.jsx.

--- src/GamePage/GamePage.jsx
+++ src/GamePage/GamePage.jsx
@@ -1,6 +1,17 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
 
+// import Header from './Header.jsx';
+// import CenterDisplay from './CenterDisplay.jsx';
+// import PlayerFrame from './PlayerFrame.jsx';
+// import LivingChat from './LivingChat.jsx';
+// import WolvesChat from './WolvesChat.jsx';
+// import ChatToggle from './ChatToggle.jsx';
+// import Voting from './Voting.jsx';
+// import Ruleset from './Ruleset.jsx';
+// import StartButton from './StartButton.jsx';
+
+
 const GamePage = () => {
 
   //Note: These states are not final in anyway.
@@ -15,7 +26,33 @@ const GamePage = () => {
 
   //TODO: Add Lifecycle methods as needed.
 
+  //TODO: create handleFunctions. Esp for GameEvents
 
+  const handlePhaseChange = () => {
+    //TODO:
+  }
+  const handleVoting = () => {
+    //TODO:
+  }
+  const handleEndOfGame = () => {
+    //TODO:
+  }
 
-  return (<div>Sus Puppies!</div>)
+  //Note: Divs are being used as place holders to avoid errors for missing components
+
+  return (
+  //TODO: Fill in components properly with handlers.
+  <div>
+    <h1>Header</h1>
+    <div>CenterDisplay</div>
+    <div>PlayerFrame</div>
+    <div>LivingChat</div>
+    <div>WolvesChat</div>
+    <div>ChatToggle</div>
+    <div>Voting</div>
+    <div>Ruleset</div>
+    <div>StartButton</div>
+  </div>)
 }
+
+export default GamePage;

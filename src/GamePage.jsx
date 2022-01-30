aad2e1aaa78379cc569456838354cb9a80eca2cb
Himmat Khalsa <himmatsinghkhalsa@gmail.com>
2022-01-30 08:25:34 -0700

    added state, props and handler notes to GamePage

--- src/GamePage.jsx
+++ src/GamePage.jsx
@@ -16,7 +16,7 @@ import Voting from './Components/Voting.jsx';
 const GamePage = () => {
 
   //Note: These states are not final in anyway.
-  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(0);
   const [previousResult, setPreviousResult] = useState('');
   const [currentDay, setCurrentDay] = useState(0);
   const [currentPhase, setCurrentPhase] = useState('');
@@ -24,34 +24,83 @@ const GamePage = () => {
   const [voting, setVoting] = useState([]);
   const [gameStatus, setGameStatus] = useState('');
   const [phaseResults, setPhaseResults] = useState([]);
+  const [playerInfo, setPlayerInfo] = useState([]);
+  const [ghostChats, setGhostChats] = useState([]);
+  const [livingChats, setLivingChats] = useState([]);
+  const [wolfChats, setWolfChats] = useState([]);
+  const [host, setHost] = useState('');
+  const [playerId, setPlayerId] = useState('');
+
 
   //TODO: Add Lifecycle methods as needed.
 
   //TODO: create handleFunctions. Esp for GameEvents
 
   const handlePhaseChange = () => {
-    //TODO:
+    //TODO: needs to update currentDay and currentPhase on server when timer runs out
   }
+  //QUESTION: are individual player votes sent individually to the server?
+  // or are they updated in GamePage to be sent up all together?
   const handleVoting = () => {
-    //TODO:
+    //TODO: needs to send voting object to the server to get phaseResults
   }
   const handleEndOfGame = () => {
-    //TODO:
+    //endgame conditions might automatically be determined from the server
+    //TODO: needs to send playerRoles (and voting?) to server to determine if game ends
+  }
+  const handlePlayerJoiningGame = () => {
+    //TODO: needs to tell server when a new player has connected
+      //assign player_id, take in user's name and user's chosen icon/image
   }
 
   //Note: Divs are being used as place holders to avoid errors for missing components
   return (
     //TODO: Fill in components properly with handlers.
     <div>
-      <Header />
-      <LivingChat />
-      <Ruleset />
-      <GameButton />
-      <Voting />
-      <WolfChat />
-      <GhostChat />
-      <Timer />
-      <PlayerFrame />
+      <Header
+        previousResult={previousResult}
+        currentDay={currentDay}
+        currentPhase={currentPhase}
+      />
+      <LivingChat
+        livingChats={livingChats}
+        playerInfo={playerInfo}
+        playerId={playerId}
+      />
+      <Ruleset
+        {/* gameLogic object containing game-specific rules (# of wolves, etc) */}
+      />
+      <GameButton
+        playerId={playerId}
+        gameStatus={gameStatus}
+      />
+      <Voting
+        timer={timer}
+        playerInfo={playerInfo}
+        currentPhase={currentPhase}
+        playerRoles={playerRoles}
+      />
+      <WolfChat
+        wolfChats={wolfChats}
+        playerInfo={playerInfo}
+        playerId={playerId}
+        playerRoles={playerRoles}
+      />
+      <GhostChat
+        ghostChats={ghostChats}
+        playerInfo={playerInfo}
+        playerId={playerId}
+        playerRoles={playerRoles}
+      />
+      <Timer
+        timer={timer}
+        currentPhase={currentPhase}
+      />
+      <PlayerFrame
+        voting={voting}
+        playerInfo={playerInfo}
+        currentPhase={currentPhase}
+      />
     </div>
   )
 }
--- src/mockData.js
+++ src/mockData.js
@@ -12,11 +12,12 @@ const data = {
     gameStatus: 'playing',
     phaseResults: [[1, 'Night', 'sha2']], // An array of tuples that will be displayed at the end game screen. [current day, string for time of day of event, player name]
     playerInfo: [{ name: 'Dare', picture: 'todo/pic', player_id: 'sha2' }, { name: 'himmat', picture: 'todo/pic', player_id: 'sha3' }, { name: 'Megan', picture: 'todo/pic', player_id: 'sha10' }, { name: 'Ryder', picture: 'todo/pic', player_id: 'sha1' }],
-    ghostChats: chatData,
-    livingChats: chatData,
-    wolfChats: chatData,
+    ghostChats: chatData, //Himmat: I'm picturing this as an array. Need to change in GamePage if different
+    livingChats: chatData, //Himmat: I'm picturing this as an array. Need to change in GamePage if different
+    wolfChats: chatData, //Himmat: I'm picturing this as an array. Need to change in GamePage if different
     host: 'sha2', // This is a string that will match Dare's id which is also a string.
-
+    //QUESTION: where will the Ruleset component get data about the current game rules?
+      //we mentioned have a GameLogic object listing the particular game rules
   },
   playerState: {
     player_id: 'host', // Asked to be a string. It could be a weird string like this: '12bx#sdr311'

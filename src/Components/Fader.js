622ad22d1d3ccbed2192ed6ce40d2fd661def0f6
Ryder Wendt <ryderw93@gmail.com>
2022-01-31 16:58:12 -0500

    added claw mechanic and other svgs

---
+++ src/Components/Fader.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import wolf from "./wolfClaw.svg";
+
+const Fader = (props) => {
+  const [fadeProp, setFadeProp] = useState({
+    fade: "fade-in",
+  });
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      if (fadeProp.fade === "fade-in") {
+        setFadeProp({
+          fade: "fade-out",
+        });
+      } else {
+        setFadeProp({
+          fade: "fade-in",
+        });
+      }
+    },2000);
+    return () => {};
+  }, [fadeProp]);
+
+  return (
+    <>
+      <img src={wolf} width={300} className={fadeProp.fade} alt="wolf" />
+    </>
+  );
+};
+
+export default Fader;

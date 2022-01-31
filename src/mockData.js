6db3e59b6204f038094539dc63023ca18962fd22
Javier Tamez <tamezjavier@gmail.com>
2022-01-30 18:37:39 -0600

    Checkpoint save.

--- src/mockData.js
+++ src/mockData.js
@@ -9,7 +9,7 @@ const data = {
     currentPhase: 'Day', // use an integer to represent current day. 0 === Day. 1 === Night.
     playerRoles: [['sha2', 0], ['sha10', 0], ['sha3', 0], ['sha1', 2]],
     voting: [['sha2', 'sha10'], ['sha10', null], ['sha3', 'sha10'], ['sha1', 'sha10']], //Null means none votes.
-    gameStatus: 'playing',
+    gameStatus: 'not started',
     phaseResults: [[1, 'Night', 'sha2']], // An array of tuples that will be displayed at the end game screen. [current day, string for time of day of event, player name]
     playerInfo: [{ name: 'Dare', picture: 'todo/pic', player_id: 'sha2' }, { name: 'himmat', picture: 'todo/pic', player_id: 'sha3' }, { name: 'Megan', picture: 'todo/pic', player_id: 'sha10' }, { name: 'Ryder', picture: 'todo/pic', player_id: 'sha1' }],
     ghostChats: chatData, //Himmat: I'm picturing this as an array. Need to change in GamePage if different

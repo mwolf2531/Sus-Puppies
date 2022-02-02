316fdcb95190def41e155e39e0cb6ad6a0f3fb42
Javier Tamez <tamezjavier@gmail.com>
2022-02-01 18:48:04 -0600

    added timer forcing vote when timer is 0. Migrated wolves count to init wolves and passed info to rule set.

--- src/Components/Ruleset.jsx
+++ src/Components/Ruleset.jsx
@@ -4,12 +4,13 @@ import Styled from 'styled-components';
 const Ruleset = ({ wolves, initialTimer, playerInfo }) => {
   // ruleset receiever
   // socket.on('ruleset-feed', object of gamesettings)
+
   console.log('WOLVES: ', wolves);
   return (
     <div>
       <h3>Ruleset</h3>
       <div>{playerInfo.length} Players</div>
-      <div>{wolves.number} Werewolves</div>
+      <div>{wolves} Werewolves</div>
       <div>{initialTimer} seconds per round</div>
     </div>
   );
